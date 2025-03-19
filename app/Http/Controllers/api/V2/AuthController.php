b4fdfdef8076a249e7389322a503eac935c66c4d
ilyass mar <ilyass.marghine@edu.uiz.ac.ma>
2025-03-19 14:13:19 +0000

    done with the refresh token fucntionality

--- app/Http/Controllers/api/V2/AuthController.php
+++ app/Http/Controllers/api/V2/AuthController.php
@@ -5,13 +5,19 @@ namespace App\Http\Controllers\api\V2;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
+
 class AuthController extends Controller
 {
+
+    public function index() {
+        return response()->json(["hello"=>"You need to log in"]);
+    }
     public function register(Request $request)
     {
         // Validate incoming request data
@@ -57,13 +63,21 @@ class AuthController extends Controller
             return response()->json(['message' => 'Invalid credentials'], 401);
         }
 
+        //delete old tokens , this is an optional step;
+        $user->tokens()->delete();
+
         // Generate token for user
         $token = $user->createToken('lemsid')->plainTextToken;
 
+        //generate a refresh token 
+        $refreshToken = $user->createToken('lemsid')->plainTextToken;
+
         return response()->json([
             'user' => $user,
-            'token' => $token
-        ]);
+            'token_type'=>'Bearer',
+            'token' => $token, 
+            'refresh_token'=>$refreshToken
+        ],200);
     }
 
     // Logout method
@@ -81,5 +95,38 @@ class AuthController extends Controller
     public function user(Request $request)
     {
         return response()->json($request->user());
+    }
+
+    public function refreshToken(Request $request){
+        $request->validate([
+            "refresh_token"=>"required",
+        ]);
+
+        //find the user by using refresh token 
+        $user = User::whereHas('tokens', function ($query) use ($request) {
+            $query->where('token', hash('sha256', $request->refresh_token));
+        })->first();
+
+        //if the user not found the show the following message;
+        if (!$user) {
+            return response()->json(['message' => 'Invalid refresh token'], 401);
+        }
+
+        //delete the old token
+        $user->tokens()->where('token',hash("sha256",$request->refresh_token))->delete();
+
+        //daba let's create the token
+        $token = $user->createToken('lemsid')->plainTextToken;
+
+        //create a refresh token
+        $refreshToken = $user->createToken('lemsid')->plainTextToken; 
+
+        return response()->json([
+            "user"=>$user,
+            "token"=>$token,
+            "refresh_token"=>$refreshToken,
+            "token_type"=>"Bearer",
+        ]);
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,19 +25,22 @@ Route::group(["prefix"=>"V1"],function (){
     Route::apiResource('Tags',TagController::class);
 });
 
-
+    Route::post('/auth/register', [AuthController::class, 'register']);
+    Route::post('/auth/login', [AuthController::class, 'login']);
+    Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::get('/auth/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+    // Route::get('/auth/login', [AuthController::class, 'index'])->name("login");
 Route::group(["prefix"=>"V2",'middleware' => ['auth:sanctum']],function (){
-
+    
+    
     Route::apiResource('Course',CourseController::class);
 
     Route::apiResource('Categories',CategoryController::class);
     
     Route::apiResource('Tags',TagController::class);
 
-    Route::post('/auth/register', [AuthController::class, 'register']);
-    Route::post('/auth/login', [AuthController::class, 'login']);
-    Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-    Route::get('/auth/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+    Route::post('auth/refresh',[AuthController::class, "refreshToken"]);
+
 
     
 });
